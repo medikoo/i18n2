5fd68dd0242612d13d0297622a562189d0d0d575
kamsi <gruca.kamil@gmail.com>
2014-08-08 11:54:53 +0200

    #1 source parser

---
+++ parse/source.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var esniff = require('esniff/function'), clearRaw, prepareResult;
+
+clearRaw = function (raw) {
+	return raw.replace(/^("|')/, '').replace(/("|')$/, '');
+};
+
+prepareResult = function (funName, fileContent, result) {
+	var clearKey, occurances;
+	occurances = esniff(funName)(fileContent);
+	occurances.forEach(function (o) {
+		clearKey = clearRaw(o.raw);
+		if (result.messages[clearKey]) {
+			result.messages[clearKey].push(o);
+		} else {
+			result.messages[clearKey] = [o];
+		}
+		delete o.raw;
+	});
+	return result;
+};
+
+module.exports = function (fileContent) {
+	var context, result = {};
+	context = fileContent.match(/i18n.bind\((.*?)\)/);
+	if (context && context[1]) {
+		context = clearRaw(context[1]);
+	}
+	result.context = context ? context : '';
+	result.messages = {};
+	result = prepareResult('_', fileContent, result);
+	result = prepareResult('_n', fileContent, result);
+
+	return JSON.stringify(result);
+};
