9e52befeee8e7b003f6403f77dcbf8b38e1bbdec
Mariusz Nowak <medyk@medikoo.com>
2015-06-01 21:00:45 +0200

    Do not duplicate es6-template-strings functionality

--- _message.js
+++ _message.js
@@ -4,25 +4,12 @@ var mixin          = require('es5-ext/object/mixin')
   , setPrototypeOf = require('es5-ext/object/set-prototype-of')
   , d              = require('d')
   , compile        = require('es6-template-strings/compile')
-  , resolve        = require('es6-template-strings/resolve')
+  , resolve        = require('es6-template-strings/resolve-to-array')
 
-  , forEach = Array.prototype.forEach
-  , Message, tag;
-
-tag = function (literals/*, …substitutions*/) {
-	var args = arguments, result = [];
-	forEach.call(literals, function (value, i) {
-		if (!i) {
-			result.push(value);
-			return;
-		}
-		result.push(args[i], value);
-	});
-	return result;
-};
+  , Message;
 
 Message = module.exports = function (template, inserts) {
-	var message = tag.apply(null, resolve(compile(template), inserts));
+	var message = resolve(compile(template), inserts);
 	if (setPrototypeOf) return setPrototypeOf(message, Message.prototype);
 	return mixin(message, Message.prototype);
 };
